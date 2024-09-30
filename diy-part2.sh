0c2b22b33e65e7ad890fb5c61d76c73b22848b41
kygsmsc <59911411+kygsmsc@users.noreply.github.com>
2024-09-30 13:08:42 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,4 +26,4 @@ sed -i 's/192.168.1.1/10.10.10.254/g' package/base-files/files/bin/config_genera
 # 添加luci-app-argon-config
 (git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config && {
     [ -d package/luci-app-argon-config ] && echo "CONFIG_PACKAGE_luci-app-argon-config=y" >> .config 
-})e
+})
