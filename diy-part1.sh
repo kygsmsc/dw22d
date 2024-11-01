cf35b78889c7da78bd210d58aa28afc1dd12734a
kygsmsc <59911411+kygsmsc@users.noreply.github.com>
2024-11-01 15:05:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -25,6 +25,7 @@
 
 # Add a feed source
 rm -rf tmp
+sed -i '$a src-git MrH723 https://github.com/MrH723/openwrt-packages' feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
