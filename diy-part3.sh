6d2a8816a11004e6b61f5350a514ace62d14e0f7
kygsmsc <59911411+kygsmsc@users.noreply.github.com>
2024-11-17 10:30:21 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -49,4 +49,4 @@
       #git fetch --tags --all  # 拉取所有标签和远程分支
       git tag    # 你可以查看所有可用的版本
       git branch -a  # 查看当前使用的版本
-      git checkout 21.02.7   # 切换到指定的版本
+      git checkout v21.02.7   # 切换到指定的版本
